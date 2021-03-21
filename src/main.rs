a95f3ce324275ef19a7f6033bb15e5a67fd3acd3
Michael Cuffaro <consulting@michaelcuffaro.com>
2021-03-21 12:34:43 -0400

    WIP preliminary thick triple implementation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 // Based on https://docs.rs/csv/1.1.3/csv/tutorial/index.html
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::io;
 use std::process;
+
+use serde_json::to_string;
 
 use rio_api::model::*;
 use rio_api::parser::TriplesParser;
@@ -38,24 +41,423 @@ fn shorten(prefixes: &Vec<Prefix>, iri: &str) -> String {
     return format!("<{}>", iri);
 }
 
-fn row2o<'a>(
-    uber_row: &'a Vec<Option<String>>,
-    stanza_stack: &'a Vec<Vec<Option<String>>>,
-    stanza_name: &'a String,
-) -> &'a Vec<Option<String>> {
-    // Start with any row that describes a non-blank subject:
-    //let uber_row = stanza_stack
-    //    .iter()
-    //    .find(|&r| !r[0].as_ref().unwrap().starts_with("_:"));
-    //eprintln!("UBER ROW: {:?}", uber_row);
-    let uber_subj = uber_row[0].as_ref().unwrap();
-    let uber_pred = &uber_row[1].as_ref().unwrap();
+fn render_owl_restriction(
+    stanza_rows: &Vec<Vec<Option<String>>>,
+    given_rows: Vec<&Vec<Option<String>>>,
+) -> HashMap<String, String> {
+    // TODO: It would be good to somehow refactor all of these function calls. Everything except
+    // for the closure is the same.
+    let target_row = {
+        if let Some(target_row) = given_rows
+            .iter()
+            .find(|r| {
+                (r[1] != Some(String::from("rdf:type")))
+                    && (r[1] != Some(String::from("owl:onProperty")))
+            })
+            .map(|&r| r.clone())
+        {
+            target_row
+        } else {
+            let v = vec![];
+            v
+        }
+    };
+    //let property_row = {
+    //    if let Some(property_row) = given_rows
+    //        .iter()
+    //        .find(|r| r[1] == Some(String::from("owl:onProperty")))
+    //        .map(|&r| r.clone())
+    //    {
+    //        property_row
+    //    } else {
+    //        let v = vec![];
+    //        v
+    //    }
+    //};
+    let rdf_type_row = {
+        if let Some(rdf_type_row) = given_rows
+            .iter()
+            .find(|r| r[1] == Some(String::from("rdf:type")))
+            .map(|&r| r.clone())
+        {
+            rdf_type_row
+        } else {
+            let v = vec![];
+            v
+        }
+    };
+    let rdf_type = {
+        if let Some(rdf_type) = rdf_type_row.get(2).and_then(|r| r.clone()) {
+            rdf_type
+        } else {
+            String::from("")
+        }
+    };
 
-    // TODO: Need to chek for none here:
-    let uber_obj = String::from("foobie"); // &uber_row[2].as_ref().unwrap();
-    eprintln!("Uber row items: {}, {}, {}", uber_subj, uber_pred, uber_obj);
+    if rdf_type != "owl:Restriction" {
+        eprintln!(
+            "ERROR Unexpected rdf type: '{}' found in OWL restriction",
+            rdf_type
+        );
+        return HashMap::new().clone();
+    }
 
-    uber_row
+    let target_pred = {
+        if let Some(target_pred) = target_row.get(1).and_then(|r| r.clone()) {
+            target_pred
+        } else {
+            String::from("")
+        }
+    };
+    let target_obj = {
+        if let Some(target_obj) = target_row.get(2).and_then(|r| r.clone()) {
+            target_obj
+        } else {
+            String::from("")
+        }
+    };
+
+    eprintln!(
+        "Rendering OWL restriction for {} for object {}",
+        target_pred, target_obj
+    );
+
+    let mut restriction = HashMap::new();
+    if target_obj.starts_with("_:") {
+        let inner_rows: Vec<_> = stanza_rows
+            .iter()
+            .filter(move |&r| r[0].as_ref() == Some(&target_obj))
+            .collect();
+        let ce = render_owl_class_expression(stanza_rows, inner_rows);
+        let ce = match to_string(&ce) {
+            Ok(ce) => ce,
+            Err(_) => String::from(""),
+        };
+        restriction.insert(String::from("object"), ce);
+        return restriction;
+    } else {
+        let mut literal = HashMap::new();
+        literal.insert(String::from("object"), target_obj);
+        return literal;
+    }
+}
+
+fn get_owl_operands(
+    stanza_rows: &Vec<Vec<Option<String>>>,
+    given_row: &Vec<Option<String>>,
+) -> Vec<HashMap<String, String>> {
+    let given_pred = {
+        if let Some(given_pred) = given_row.get(1).and_then(|r| r.clone()) {
+            given_pred
+        } else {
+            String::from("")
+        }
+    };
+    let given_obj = {
+        if let Some(given_obj) = given_row.get(2).and_then(|r| r.clone()) {
+            given_obj
+        } else {
+            String::from("")
+        }
+    };
+
+    eprintln!("Finding operands for row with predicate: {}", given_pred);
+
+    if !given_obj.starts_with("_:") {
+        eprintln!("Found non-blank operand: {}", given_obj);
+        let mut non_blank = HashMap::new();
+        non_blank.insert(String::from("object"), given_obj);
+        return vec![non_blank];
+    }
+
+    let inner_rows: Vec<_> = stanza_rows
+        .iter()
+        .filter(move |&r| r[0].as_ref() == Some(&given_obj))
+        .collect();
+
+    let mut operands = Vec::new();
+    for inner_row in inner_rows.iter() {
+        let inner_subj = {
+            if let Some(inner_subj) = inner_row.get(0).and_then(|r| r.clone()) {
+                inner_subj
+            } else {
+                String::from("")
+            }
+        };
+        let inner_pred = {
+            if let Some(inner_pred) = inner_row.get(1).and_then(|r| r.clone()) {
+                inner_pred
+            } else {
+                String::from("")
+            }
+        };
+        let inner_obj = {
+            if let Some(inner_obj) = inner_row.get(2).and_then(|r| r.clone()) {
+                inner_obj
+            } else {
+                String::from("")
+            }
+        };
+
+        eprintln!(
+            "Found row with <s,p,o> = <{}, {}, {}>",
+            inner_subj, inner_pred, inner_obj
+        );
+
+        if inner_pred == "rdf:type" {
+            if inner_obj == "owl:Restriction" {
+                let operand = render_owl_restriction(stanza_rows, inner_rows);
+                operands.push(operand);
+                break;
+            } else if inner_obj == "owl:Class" {
+                let ce = render_owl_class_expression(stanza_rows, inner_rows);
+                let ce = match to_string(&ce) {
+                    Ok(ce) => ce,
+                    Err(_) => String::from(""),
+                };
+                let mut operand = HashMap::new();
+                operand.insert(String::from("object"), ce);
+                operands.push(operand);
+                break;
+            }
+        } else if inner_pred == "rdf:rest" {
+            if inner_obj != "rdf:nil" {
+                let inner_operands = get_owl_operands(stanza_rows, inner_row);
+                for operand in inner_operands.clone() {
+                    operands.push(operand);
+                }
+            }
+            eprintln!("Returned from recursing on {}", inner_pred);
+        } else if inner_pred == "rdf:first" {
+            if inner_obj.starts_with("_:") {
+                eprintln!("{} points to a blank node, following the trail", inner_pred);
+                let inner_operands = get_owl_operands(stanza_rows, inner_row);
+                for operand in inner_operands.clone() {
+                    operands.push(operand);
+                }
+                eprintln!("Returned from recursing on {}", inner_pred);
+            } else {
+                eprintln!("Rendering non-blank object with predicate: {}", inner_pred);
+                let mut operand = HashMap::new();
+                operand.insert(String::from("object"), inner_obj);
+                operands.push(operand);
+            }
+        }
+    }
+
+    return operands;
+}
+
+fn render_owl_class_expression(
+    stanza_rows: &Vec<Vec<Option<String>>>,
+    given_rows: Vec<&Vec<Option<String>>>,
+) -> HashMap<String, Vec<HashMap<String, String>>> {
+    let rdf_type_row = {
+        if let Some(rdf_type_row) = given_rows
+            .iter()
+            .find(|r| r[1] == Some(String::from("rdf:type")))
+            .map(|&r| r.clone())
+        {
+            rdf_type_row
+        } else {
+            let v = vec![];
+            v
+        }
+    };
+    let class_row = {
+        if let Some(class_row) = given_rows
+            .iter()
+            .find(|r| match &r[1] {
+                Some(pred) => pred.starts_with("owl:"),
+                None => false,
+            })
+            .map(|&r| r.clone())
+        {
+            class_row
+        } else {
+            let v = vec![];
+            v
+        }
+    };
+
+    eprintln!("Found rows: {:?}, {:?}", rdf_type_row, class_row);
+
+    let rdf_type_pred = {
+        if let Some(rdf_type_pred) = rdf_type_row.get(1).and_then(|r| r.clone()) {
+            rdf_type_pred
+        } else {
+            String::from("")
+        }
+    };
+    let rdf_type_obj = {
+        if let Some(rdf_type_obj) = rdf_type_row.get(2).and_then(|r| r.clone()) {
+            rdf_type_obj
+        } else {
+            String::from("")
+        }
+    };
+
+    let mut ce = HashMap::new();
+    let mut rdf_part = HashMap::new();
+    rdf_part.insert(String::from("object"), rdf_type_obj);
+    ce.insert(rdf_type_pred, vec![rdf_part]);
+
+    let class_subj = {
+        if let Some(class_subj) = class_row.get(0).and_then(|r| r.clone()) {
+            class_subj
+        } else {
+            String::from("")
+        }
+    };
+    let class_pred = {
+        if let Some(class_pred) = class_row.get(1).and_then(|r| r.clone()) {
+            class_pred
+        } else {
+            String::from("")
+        }
+    };
+    let class_obj = {
+        if let Some(class_obj) = class_row.get(2).and_then(|r| r.clone()) {
+            class_obj
+        } else {
+            String::from("")
+        }
+    };
+
+    let operands = get_owl_operands(stanza_rows, &class_row);
+
+    eprintln!(
+        "Rendering <s,p,o> = <{}, {}, {}>",
+        class_subj, class_pred, class_obj
+    );
+    if vec![
+        "owl:intersectionOf",
+        "owl:unionOf",
+        "owl:complementOf",
+        "owl:oneOf",
+    ]
+    .iter()
+    .any(|&i| i == class_pred.as_str())
+    {
+        ce.insert(class_pred, operands);
+    } else if class_pred.as_str() == "owl:onProperty" {
+        ce.insert(
+            class_pred,
+            vec![render_owl_restriction(stanza_rows, given_rows)],
+        );
+    } else if class_obj.starts_with("<") {
+        let mut class_obj_map = HashMap::new();
+        class_obj_map.insert(String::from("object"), class_obj);
+        ce.insert(class_pred, vec![class_obj_map]);
+    }
+
+    return ce;
+}
+
+fn row2o(
+    uber_row: &Vec<Option<String>>,
+    stanza_rows: &Vec<Vec<Option<String>>>,
+) -> Vec<Option<String>> {
+    // A handy closure for getting the value of the ith column of the uber row:
+    let get_uber_column = |index: usize| -> &str {
+        match &uber_row[index].as_ref() {
+            Some(obj) => obj,
+            None => "",
+        }
+    };
+
+    let uber_subj = String::from(get_uber_column(0));
+    let uber_pred = String::from(get_uber_column(1));
+    let uber_obj = String::from(get_uber_column(2));
+    let uber_val = String::from(get_uber_column(3));
+    eprintln!(
+        "Called row2o on <s,p,o> = <{}, {}, {}>",
+        uber_subj, uber_pred, uber_obj
+    );
+
+    if uber_obj.is_empty() {
+        if uber_val.is_empty() {
+            eprintln!("ERROR: Received empty object with empty value");
+        } else {
+            eprintln!("Rendering empty object with value: {}", uber_val);
+        }
+        return uber_row.clone();
+    } else if uber_obj.starts_with("<") {
+        eprintln!("Rendering literal IRI: {}", uber_obj);
+        return uber_row.clone();
+    } else if !uber_obj.starts_with("_:") {
+        eprintln!(
+            "Rendering non-blank triple: <s,p,o> = <{}, {}, {}>",
+            uber_subj, uber_pred, uber_obj
+        );
+        return uber_row.clone();
+    } else {
+        eprintln!(
+            "Rendering triple with blank object: <s,p,o> = <{}, {}, {}>",
+            uber_subj, uber_pred, uber_obj
+        );
+
+        let inner_rows: Vec<_> = stanza_rows
+            .iter()
+            .filter(|&r| r[0].as_ref() == Some(&uber_obj))
+            .collect();
+
+        let object_type_row = {
+            if let Some(object_type_row) = inner_rows
+                .iter()
+                .find(|r| r[1] == Some(String::from("rdf:type")))
+                .map(|&r| r.clone())
+            {
+                object_type_row
+            } else {
+                let v = vec![];
+                v
+            }
+        };
+        let object_type = {
+            if let Some(object_type) = object_type_row.get(2).and_then(|r| r.clone()) {
+                object_type
+            } else {
+                String::from("")
+            }
+        };
+
+        match object_type.as_str() {
+            "owl:Class" => {
+                eprintln!("Rendering OWL class pointed to by {}", uber_obj);
+                let ce = render_owl_class_expression(stanza_rows, inner_rows);
+                let ce = match to_string(&ce) {
+                    Ok(ce) => ce,
+                    Err(_) => String::from(""),
+                };
+                let mut row_to_return = uber_row.clone();
+                row_to_return[2] = Some(ce);
+                return row_to_return;
+            }
+            "owl:Restriction" => {
+                eprintln!("Rendering OWL restriction pointed to by {}", uber_obj);
+                let restr = render_owl_restriction(stanza_rows, inner_rows);
+                let restr = match to_string(&restr) {
+                    Ok(restr) => restr,
+                    Err(_) => String::from(""),
+                };
+                let mut row_to_return = uber_row.clone();
+                row_to_return[2] = Some(restr);
+                return row_to_return;
+            }
+            "" => {
+                eprintln!("WARNING Could not determine object type for {}", uber_pred);
+                return uber_row.clone();
+            }
+            _ => {
+                eprintln!(
+                    "WARNING Unrecognised object type: {} for predicate {}",
+                    object_type, uber_pred
+                );
+                return uber_row.clone();
+            }
+        }
+    }
 }
 
 fn get_rows_to_insert(
@@ -63,11 +465,7 @@ fn get_rows_to_insert(
     stanza_name: &mut String,
 ) -> Vec<Vec<Option<String>>> {
     let mut rows: Vec<Vec<Option<String>>> = [].to_vec();
-    //eprintln!("Thickening stanza {}", stanza_name);
-
-    eprintln!("Stanza is: {}", stanza_name);
     for s in stanza_stack.iter() {
-        //eprintln!("Thin row: {:?}", s);
         if stanza_name == "" {
             if let Some(ref sb) = s[1] {
                 *stanza_name = sb.clone();
@@ -75,9 +473,8 @@ fn get_rows_to_insert(
             }
         }
         let mut v = vec![Some(stanza_name.to_string())];
-        let s = row2o(&s, &stanza_stack, &stanza_name);
+        let s = row2o(&s, &stanza_stack);
         v.extend_from_slice(&s);
-        eprintln!("Inserting row: {:?}", v);
         rows.push(v);
     }
 
@@ -121,10 +518,11 @@ fn insert(db: &String) -> Result<(), Box<dyn Error>> {
     RdfXmlParser::new(stdin.lock(), filename.as_str())
         .unwrap()
         .parse_all(&mut |t| {
-            //eprintln!("Processing triple: {}", t);
             if t.subject == stanza_end {
-                //eprintln!("Reached the end of the stanza: {}", stanza);
-                for row in get_rows_to_insert(&mut stack, &mut stanza) {
+                for mut row in get_rows_to_insert(&mut stack, &mut stanza) {
+                    if row.len() != 7 {
+                        row.resize_with(7, Default::default);
+                    }
                     let mut stmt = tx
                         .prepare_cached(
                             "INSERT INTO statements values (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
@@ -132,6 +530,8 @@ fn insert(db: &String) -> Result<(), Box<dyn Error>> {
                         .expect("Statement ok");
                     stmt.execute(row).expect("Insert row");
                 }
+                // In the current implementation, get_rows_to_insert() will clear the stack as a
+                // side effect, so we make sure to clear it here to get ready for the next stanza:
                 stanza = String::from("");
                 stack.clear()
             } else {
