4043b696712c7f5a4e30fe27536825d1faf554a2
Michael Cuffaro <consulting@michaelcuffaro.com>
2021-04-02 17:12:52 -0400

    WIP implement dependencies portion of thin2subjects function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 // Based on https://docs.rs/csv/1.1.3/csv/tutorial/index.html
-use std::collections::HashMap;
+use std::collections::{BTreeMap, BTreeSet};
 use std::env;
 use std::error::Error;
 use std::io;
 use std::process;
 
 use serde::Serialize;
-use serde_json::to_string;
+use serde_json::{to_string, to_string_pretty};
 
 use rio_api::model::*;
 use rio_api::parser::TriplesParser;
@@ -50,7 +50,7 @@ fn thinify(
         if stanza_name == "" {
             if let Some(ref sb) = s[1] {
                 *stanza_name = sb.clone();
-                //eprintln!("Changing stanza name to {}", stanza_name);
+                //println!("Changing stanza name to {}", stanza_name);
             }
         }
         let mut v = vec![Some(stanza_name.to_string())];
@@ -60,44 +60,22 @@ fn thinify(
     return rows;
 }
 
-fn row2object_map(row: Vec<Option<String>>) -> HashMap<String, String> {
-    let object = {
-        if let Some(object) = row.get(3).and_then(|r| r.clone()) {
-            object
-        }
-        else {
-            String::from("")
-        }
-    };
-    let value = {
-        if let Some(value) = row.get(4).and_then(|r| r.clone()) {
-            value
-        }
-        else {
-            String::from("")
-        }
-    };
-    let datatype = {
-        if let Some(datatype) = row.get(5).and_then(|r| r.clone()) {
-            datatype
-        }
-        else {
-            String::from("")
-        }
-    };
-    let language = {
-        if let Some(language) = row.get(6).and_then(|r| r.clone()) {
-            language
-        }
-        else {
-            String::from("")
-        }
-    };
+fn get_column_contents(c: Option<&String>) -> String {
+    match c {
+        Some(s) => s.to_string(),
+        None => String::from(""),
+    }
+}
 
-    let mut object_map = HashMap::new();
+fn row2object_map(row: Vec<Option<String>>) -> BTreeMap<String, String> {
+    let object = get_column_contents(row[3].as_ref());
+    let value = get_column_contents(row[4].as_ref());
+    let datatype = get_column_contents(row[5].as_ref());
+    let language = get_column_contents(row[6].as_ref());
+
+    let mut object_map = BTreeMap::new();
     if object != "" {
         object_map.insert(String::from("object"), object);
-        return object_map;
     }
     else if value != "" {
         object_map.insert(String::from("value"), value);
@@ -107,109 +85,115 @@ fn row2object_map(row: Vec<Option<String>>) -> HashMap<String, String> {
         else if language != "" {
             object_map.insert(String::from("language"), language);
         }
-        return object_map;
     }
     else {
         // TODO: The python code throws an exception here. Should we do something similar?
-        eprintln!("ERROR: Invalid RDF row");
-        return HashMap::new().clone();
+        println!("ERROR: Invalid RDF row");
     }
+
+    return object_map;
 }
 
 fn thin2subjects(
     thin_rows: &Vec<Vec<Option<String>>>,
-) -> HashMap<String, HashMap<String, Vec<HashMap<String, String>>>> {
-    let mut subject_ids = vec![];
+) -> BTreeMap<String, BTreeMap<String, Vec<BTreeMap<String, String>>>> {
+    let mut subjects = BTreeMap::new();
+    let mut dependencies: BTreeMap<String, BTreeSet<_>> = BTreeMap::new();
+    let mut subject_ids: BTreeSet<String> = vec![].into_iter().collect();
     for row in thin_rows.iter() {
-        let subject_id = row[1].clone().unwrap_or(String::from(""));
-        if subject_id != "" && !subject_ids.contains(&subject_id) {
-            subject_ids.push(subject_id);
-        }
+        subject_ids.insert(row[1].clone().unwrap_or(String::from("")));
     }
 
-    let mut subjects = HashMap::new();
-    let mut dependencies: HashMap<String, Vec<_>> = HashMap::new();
     for subject_id in subject_ids.iter() {
-        let mut predicates: HashMap<String, Vec<_>> = HashMap::new();
+        let mut predicates: BTreeMap<String, Vec<_>> = BTreeMap::new();
         for row in thin_rows.iter() {
-            if subject_id.to_string() != row[1].clone().unwrap_or(String::from("")) {
+            if subject_id.to_string() != get_column_contents(row[1].as_ref()) {
                 continue;
             }
 
-            let predicate = row[2].clone().unwrap_or(String::from(""));
-            if let Some(v) = predicates.get_mut(&predicate) {
+            let add_objects_and_sort = |v: &mut Vec<_>| {
                 v.push(row2object_map(row.to_vec()));
                 v.sort_by(|a, b| {
                     let a = to_string(&a).unwrap_or(String::from(""));
                     let b = to_string(&b).unwrap_or(String::from(""));
                     a.cmp(&b)
                 });
+            };
+
+            let predicate = get_column_contents(row[2].as_ref());
+            if let Some(v) = predicates.get_mut(&predicate) {
+                add_objects_and_sort(v);
             }
             else if predicate != "" {
-                predicates.insert(predicate, vec![]);
+                let mut v = vec![];
+                add_objects_and_sort(&mut v);
+                predicates.insert(predicate, v);
             }
             else {
-                eprintln!("WARNING row {:?} has empty predicate", row);
+                println!("WARNING row {:?} has empty predicate", row);
             }
 
-            let object = row[3].clone().unwrap_or(String::from(""));
+            let object = get_column_contents(row[3].as_ref());
             if object != "" && object.starts_with("_:") {
                 if let Some(v) = dependencies.get_mut(subject_id) {
-                    // add uniquely to v
-                    if !v.contains(&object) {
-                        v.push(object);
-                    }
+                    v.insert(object);
                 }
                 else {
-                    dependencies.insert(subject_id.to_string(), vec![object]);
+                    let mut v = BTreeSet::new();
+                    v.insert(object);
+                    dependencies.insert(subject_id.to_string(), v);
                 }
             }
         }
-        //eprintln!("Predicates of {:?}: {:?}", subject_id, predicates);
         subjects.insert(subject_id.to_string(), predicates);
     }
+    println!("SUBJECTS ARE:\n {}", to_string_pretty(&subjects).unwrap());
 
     // Work from leaves to root, nesting the blank structures:
     while !dependencies.is_empty() {
-        let mut leaves = vec![];
+        let mut leaves: BTreeSet<String> = vec![].into_iter().collect();
         for leaf in subjects.keys() {
             if !dependencies.keys().collect::<Vec<_>>().contains(&leaf) {
-                leaves.push(leaf.clone());
+                leaves.insert(leaf.clone());
             }
         }
 
         dependencies.clear();
-        let mut handled = vec![];
-        let mut subjects_tmp = HashMap::new();
-        for (subject_id, predicates) in subjects.iter() {
-            for predicate in predicates.keys() {
+        let mut handled = BTreeSet::new();
+        for subject_id in subjects.clone().keys() {
+            let mut predicates = subjects.get(subject_id).unwrap_or(&BTreeMap::new()).clone();
+            for predicate in predicates.clone().keys() {
                 let mut objects = vec![];
                 for obj in predicates.get(predicate).unwrap_or(&vec![]) {
+                    let mut obj = obj.clone();
                     let o = obj.get(&String::from("object"));
                     match o {
+                        None => {}
                         Some(o) => {
+                            let o = o.clone();
                             if o.starts_with("_:") {
                                 if leaves.contains(&o) {
-                                    let val = subjects.get(o).unwrap_or(&HashMap::new()).clone();
-                                    let mut complex_obj = HashMap::new();
-                                    complex_obj.insert(String::from("object"), val);
-                                    if !handled.contains(o) {
-                                        handled.push(o.clone());
-                                    }
+                                    // TODO: Instead of converting to a String here, we should
+                                    // create a complex object. This will require us to redefine
+                                    // predicates so that it is a map from Strings to possibly
+                                    // nested objects.
+                                    let val = subjects.get(&o).unwrap_or(&BTreeMap::new()).clone();
+                                    let val = to_string(&val).unwrap_or(String::from(""));
+                                    obj.insert(String::from("object"), val);
+                                    handled.insert(o);
                                 }
                                 else {
                                     if let Some(v) = dependencies.get_mut(subject_id) {
-                                        if !v.contains(o) {
-                                            v.push(o.clone());
-                                        }
+                                        v.insert(o);
                                     }
                                     else {
-                                        dependencies.insert(subject_id.clone(), vec![o.clone()]);
+                                        let mut v = BTreeSet::new();
+                                        v.insert(o);
+                                        dependencies.insert(subject_id.clone(), v);
                                     }
                                 }
                             }
                         }
-                        None => {}
                     }
                     objects.push(obj.clone());
                 }
@@ -218,28 +202,32 @@ fn thin2subjects(
                     let b = to_string(&b).unwrap_or(String::from(""));
                     a.cmp(&b)
                 });
-                let mut predicates_tmp = predicates.clone();
-                predicates_tmp.insert(predicate.clone(), objects);
-                subjects_tmp.insert(subject_id.clone(), predicates_tmp);
+                predicates.insert(predicate.to_string(), objects);
+                subjects.insert(subject_id.to_string(), predicates.clone());
             }
         }
-        subjects = subjects_tmp.clone();
-        for subject_id in handled {
-            subjects.remove(&subject_id);
+        for subject_id in &handled {
+            subjects.remove(subject_id);
         }
     }
+
+    println!(
+        "SUBJECTS ARE NOW:\n {}",
+        to_string_pretty(&subjects).unwrap()
+    );
 
     // TODO: Handle OWL annotations and RDF reification
     //...
 
-    return subjects;
+    //return subjects;
+    return BTreeMap::new();
 }
 
-fn render_subjects(subjects: HashMap<String, HashMap<String, Vec<HashMap<String, String>>>>) {
+fn render_subjects(subjects: BTreeMap<String, BTreeMap<String, Vec<BTreeMap<String, String>>>>) {
     let mut subject_ids: Vec<_> = subjects.keys().collect();
     subject_ids.sort();
     for subject_id in subject_ids {
-        eprintln!("{}", subject_id);
+        println!("{}", subject_id);
         let predicates = subjects.get(subject_id);
         let mut pkeys: Vec<_> = match predicates {
             Some(p) => p.keys().collect(),
@@ -247,32 +235,16 @@ fn render_subjects(subjects: HashMap<String, HashMap<String, Vec<HashMap<String,
         };
         pkeys.sort();
         for pkey in pkeys {
-            eprintln!(" {}", pkey);
+            println!(" {}", pkey);
             let objs = match predicates {
                 Some(p) => p.get(pkey).unwrap().clone(),
                 None => vec![],
             };
             for obj in objs {
-                eprintln!("   {:?}", obj);
+                println!("   {:?}", obj);
             }
         }
     }
-}
-
-fn get_rows_to_insert(
-    stanza_stack: &mut Vec<Vec<Option<String>>>, stanza_name: &mut String,
-) -> Vec<Vec<Option<String>>> {
-    //eprintln!("Processing stanza: {} ...", stanza_name);
-
-    let thin_rows = thinify(stanza_stack, stanza_name);
-    let subjects = thin2subjects(&thin_rows);
-    eprintln!("#############################################");
-    eprintln!("{:?}", subjects);
-    eprintln!("#############################################");
-    render_subjects(subjects);
-
-    // TODO: replace this later with thickified rows:
-    return thin_rows;
 }
 
 fn insert(db: &String) -> Result<(), Box<dyn Error>> {
@@ -309,24 +281,20 @@ fn insert(db: &String) -> Result<(), Box<dyn Error>> {
         params![],
     )?;
     let filename = format!("file:{}", db);
+    let mut thin_rows: Vec<_> = vec![];
     RdfXmlParser::new(stdin.lock(), filename.as_str())
         // TODO: Check with James if it would be better to replace the call to unwrap() with a
         // more robust error handling mechanism.
         .unwrap()
         .parse_all(&mut |t| {
             if t.subject == stanza_end {
-                for mut row in get_rows_to_insert(&mut stack, &mut stanza) {
+                for mut row in thinify(&mut stack, &mut stanza) {
                     if row.len() != 7 {
                         row.resize_with(7, Default::default);
                     }
-                    let mut stmt = tx
-                        .prepare_cached(
-                            "INSERT INTO statements values (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
-                        )
-                        .expect("Statement ok");
-                    stmt.execute(row).expect("Insert row");
+                    thin_rows.push(row);
                 }
-                // In the current implementation, get_rows_to_insert() will clear the stack as a
+                // In the current implementation, thinify() will clear the stack as a
                 // side effect, so we make sure to clear it here to get ready for the next stanza:
                 stanza = String::from("");
                 stack.clear()
@@ -378,6 +346,25 @@ fn insert(db: &String) -> Result<(), Box<dyn Error>> {
         // TODO: Check with James if it would be better to replace the call to unwrap() with a
         // more robust error handling mechanism.
         .unwrap();
+
+    //println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    //println!("Received {} rows:\n{}", thin_rows.len(),
+    //         to_string_pretty(&thin_rows).unwrap());
+    //println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+
+    let subjects = thin2subjects(&thin_rows);
+    //println!("#############################################");
+    //println!("{}", to_string_pretty(&subjects).unwrap());
+    //println!("#############################################");
+    // render_subjects(subjects);
+
+    for row in thin_rows {
+        let mut stmt = tx
+            .prepare_cached("INSERT INTO statements values (?1, ?2, ?3, ?4, ?5, ?6, ?7)")
+            .expect("Statement ok");
+        stmt.execute(row).expect("Insert row");
+    }
+
     tx.commit()?;
     Ok(())
 }
